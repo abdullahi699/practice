37978dd775e058237d92cdd9f14e0d35f5ac0e99
Abdullahi Muhammad <abdullahibmuhammad69@gmail.com>
2023-11-22 17:35:53 +0100

    first commit

---
+++ submit_.php
@@ -0,0 +1,46 @@
+<?php
+
+
+$firstname = $_POST["firstname"];
+$lastname = $_POST["lastname"];
+$email = $_POST["email"];
+$password = $_POST["password"];
+
+echo "your first name is " .  $firstname."<br>";
+echo "your last name is " .  $lastname."<br>";
+echo "your email is " .  $email."<br>";
+echo "your password is " . $password;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+?>
